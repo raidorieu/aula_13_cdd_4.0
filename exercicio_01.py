f1a337efab16905e3dbc92f22435b25c1b95d1b3
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-10-10 10:35:43 -0300

    exercicio

---
+++ exercicio_01.py
@@ -0,0 +1,4 @@
+from biblioteca import *
+numero=int(input("Digite um número: "))
+resposta=posneg(numero)
+print(resposta)
---
+++ exercicio_02.py
@@ -0,0 +1,6 @@
+from biblioteca import *
+nota1=4
+nota2=10
+
+n1=media(nota1, nota2)
+aprovado(n1)
